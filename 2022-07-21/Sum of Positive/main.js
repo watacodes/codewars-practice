f5eceb500137edf40d5e949ee60d31a38d4037db
Wataru Hori <Bireleysaddiction@gmail.com>
2022-07-21 07:23:49 +0900

    Solved question, 8k

---
+++ 2022-07-21/Sum of Positive/main.js
@@ -0,0 +1,23 @@
+/* 
+
+Codewars Challenge, 8k: https://www.codewars.com/kata/5715eaedb436cf5606000381/train/javascript
+
+You get an array of numbers, return the sum of all of the positives ones.
+
+Example [1,-4,7,12] => 1 + 7 + 12 = 20
+
+Note: if there is nothing to sum, the sum is default to 0.
+
+*/
+
+function positiveSum(arr) {
+    // First, if we have no value in arr, return 0
+    // Then we have to ignore the negative numbers.
+    // After that, we have to add all numbers and
+    // return the value
+    let result = 0;   
+    arr.map(x => !(x < 0) ? result += x : result += 0);
+    return result;
+  }
+
+  console.log(positiveSum([]));
