e27744b8c134465afd899f330aa9b7ff7df7f668
Wataru Hori <Bireleysaddiction@gmail.com>
2022-07-22 13:50:15 +0900

    Solved question

---
+++ 2022-07-22/Convert a Boolean into a String/main.js
@@ -0,0 +1,14 @@
+/*
+
+Codewars challenge, 8k: https://www.codewars.com/kata/551b4501ac0447318f0009cd
+
+
+Implement a function which convert the given boolean value into its string representation.
+
+Note: Only valid inputs will be given.
+
+*/
+
+function booleanToString(b){
+    return b.toString();
+}
