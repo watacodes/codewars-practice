e76c31cda09a98924e89edc99f53d74fc0cdb79c
Wataru Hori <Bireleysaddiction@gmail.com>
2022-07-22 13:59:59 +0900

    Solved question, 8k

---
+++ 2022-07-22/Find the position!/main.js
@@ -0,0 +1,19 @@
+/* 
+
+Codewars question, 8k https://www.codewars.com/kata/5808e2006b65bff35500008f
+
+When provided with a letter, return its position in the alphabet.
+
+Input :: "a"
+
+Ouput :: "Position of alphabet: 1"
+
+*/
+
+function position(letter){
+    // 1. Identify the charcode of letter for an alphabet a
+    // 2. the charcode of a - number should be 1.
+    // 3. return letter.charCodeAt() - number.
+    let pos = letter.charCodeAt() - 96;
+    return `Position of alphabet: ${pos}`;
+}
