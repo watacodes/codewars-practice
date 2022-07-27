0c3699865b99da84b2aa19c479d3756344a27302
Wataru Hori <Bireleysaddiction@gmail.com>
2022-07-28 08:13:04 +0900

    Solved question, 8k

---
+++ 2022-07-28/You Can't Code Under Pressure/main.js
@@ -0,0 +1,10 @@
+/*
+
+Codewars, 8k: https://www.codewars.com/kata/53ee5429ba190077850011d4
+
+Code as fast as you can! You need to double the integer and return it.
+
+
+*/
+
+const doubleInteger = i => i * 2;
