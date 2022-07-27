0aeea65fac17b32dab86d83ede45a105b19d5839
Wataru Hori <Bireleysaddiction@gmail.com>
2022-07-28 08:26:50 +0900

    Solved question, 8k

---
+++ 2022-07-28/Grasshopper - Personalized Message/main.js
@@ -0,0 +1,15 @@
+/*
+
+Codewars 8k: https://www.codewars.com/kata/5772da22b89313a4d50012f7
+
+Create a function that gives a personalized greeting. This function takes two parameters: name and owner.
+
+Use conditionals to return the proper message:
+
+case	return
+name equals owner	'Hello boss'
+otherwise	'Hello guest'
+
+*/
+
+const greet = (name, owner) => name === owner ? `Hello boss` : `Hello guest`;
