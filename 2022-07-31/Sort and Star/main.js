eff7019c31514a8a961e2d8eb2f952331324e3e5
Wataru Hori <Bireleysaddiction@gmail.com>
2022-07-31 13:44:24 +0900

    Solved question

---
+++ 2022-07-31/Sort and Star/main.js
@@ -0,0 +1,16 @@
+/*
+
+Codewars 8k: https://www.codewars.com/kata/57cfdf34902f6ba3d300001e
+
+You will be given a vector of strings. You must sort it alphabetically (case-sensitive, and based on the ASCII values of the chars) and then return the first value.
+
+The returned value must be a string, and have "***" between each of its letters.
+
+You should not remove or add elements from/to the array.
+
+*/
+
+const twoSort = s => s.sort()[0].split('').join('***');
+
+
+console.log(twoSort(["bitcoin", "take", "over", "the", "world", "maybe", "who", "knows", "perhaps"]))
