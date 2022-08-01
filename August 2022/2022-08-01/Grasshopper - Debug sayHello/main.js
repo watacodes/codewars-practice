b6090c0ac97c367fdbd0be575c472a923610dddc
Wataru Hori <Bireleysaddiction@gmail.com>
2022-08-01 15:16:51 +0900

    Solved question, 8k

---
+++ August 2022/2022-08-01/Grasshopper - Debug sayHello/main.js
@@ -0,0 +1,15 @@
+/*
+
+Codewars 8k: https://www.codewars.com/kata/5625618b1fe21ab49f00001f
+
+The starship Enterprise has run into some problem when creating a program to greet everyone as they come aboard. It is your job to fix the code and get the program working again!
+
+Example output:
+
+Hello, Mr. Spock
+
+*/
+
+function sayHello(name) {
+  return `Hello, ${name}`;
+};
