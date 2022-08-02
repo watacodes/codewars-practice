d1050eeb13bad6838df0f9eef78f8b59f13c81c9
Wataru Hori <Bireleysaddiction@gmail.com>
2022-08-02 19:59:56 +0900

    Solved question, 8k

---
+++ August 2022/2022-08-02/Function 2 - Squaring an argument/main.js
@@ -0,0 +1,10 @@
+/*
+
+Codewars 8k: https://www.codewars.com/kata/523b623152af8a30c6000027
+
+  Now you have to write a function that takes an argument and returns the square of it.
+
+
+*/
+
+const square = n => n ** 2;
