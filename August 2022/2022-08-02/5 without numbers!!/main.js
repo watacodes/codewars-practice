6290b811cc74c87d8568077a1342f3f101195a9e
Wataru Hori <Bireleysaddiction@gmail.com>
2022-08-02 20:30:59 +0900

    Solved question, 8k

---
+++ August 2022/2022-08-02/5 without numbers!!/main.js
@@ -0,0 +1,16 @@
+/*
+
+Codewars 8k: codewars.com/kata/59441520102eaa25260000bf
+
+
+Write a function that always returns 5
+
+Sounds easy right? Just bear in mind that you can't use any of the following characters: 0123456789*+-/
+
+*/
+
+const unusualFive = () => {
+  return 'itsMe'.length;
+}
+
+console.log(unusualFive());
