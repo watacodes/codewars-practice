e49dad5d2d90139bba09ad10d749388ca9812dfa
Wataru Hori <Bireleysaddiction@gmail.com>
2022-08-02 19:53:50 +0900

    Solved question, 8k

---
+++ August 2022/2022-08-02/Training JS #7: if..else and ternary operator/main.js
@@ -0,0 +1,12 @@
+/*
+
+Codewars 8k: 
+
+Training JS #7: if..else and ternary operator
+
+
+*/
+
+const saleHotdogs = n => n >= 5 && n < 10 ? n * 95 : n < 5 ? n * 100 : n * 90;
+
+console.log(saleHotdogs(5));
