55d7e98cc175584a8d9c07cf31ea1fc53b2cb3ae
Wataru Hori <Bireleysaddiction@gmail.com>
2022-08-03 10:46:15 +0900

    Solved Question, 8k

---
+++ August 2022/2022-08-03/Basic Variables Assignment/main.js
@@ -0,0 +1,11 @@
+/*
+
+Codewars 8k: https://www.codewars.com/kata/50ee6b0bdeab583673000025
+
+
+This code should store "codewa.rs" as a variable called name but it's not working. Can you figure out why?
+*/
+
+let a = "code";
+let b = "wa.rs";
+let name = a + b;
