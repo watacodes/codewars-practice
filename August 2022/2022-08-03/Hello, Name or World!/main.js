6f3d25054b2f34d2f1f7b9601cb0ef8494d3159a
Wataru Hori <Bireleysaddiction@gmail.com>
2022-08-03 11:38:15 +0900

    Solved Question, 8k

---
+++ August 2022/2022-08-03/Hello, Name or World!/main.js
@@ -0,0 +1,20 @@
+/*
+
+Codewars, 8k:
+
+Define a method hello that returns "Hello, Name!" to a given name, or says Hello, World! if name is not given (or passed as an empty String).
+
+Assuming that name is a String and it checks for user typos to return a name with a first capital letter (Xxxx).
+
+Examples:
+
+* With `name` = "john"  => return "Hello, John!"
+* With `name` = "aliCE" => return "Hello, Alice!"
+* With `name` not given 
+  or `name` = ""        => return "Hello, World!"
+
+*/
+
+const hello = name => !name ? "Hello, World!" : `Hello, ${name.charAt(0).toUpperCase() + name.toLowerCase().slice(1, name.length + 1)}!`
+
+console.log(hello('WATARU'));
