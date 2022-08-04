c5fc14813a3548c821b02c7202991c32d5cf7432
Wataru Hori <Bireleysaddiction@gmail.com>
2022-08-04 15:45:51 +0900

    Solved Question, 8k

---
+++ August 2022/2022-08-04/Bin to Decimal/main.js
@@ -0,0 +1,12 @@
+/*
+
+Codewars, 8k: 
+
+Complete the function which converts a binary number (given as a string) to a decimal number.
+
+
+*/
+
+const binToDec = b => parseInt(b, 2);
+
+console.log(binToDec(10));
