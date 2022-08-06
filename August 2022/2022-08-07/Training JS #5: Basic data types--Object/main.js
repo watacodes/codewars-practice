56d0f218fbd6ab7ebc087379385b5a0b5a7206df
Wataru Hori <Bireleysaddiction@gmail.com>
2022-08-07 01:06:37 +0900

    Solved Question, 8k

---
+++ August 2022/2022-08-07/Training JS #5: Basic data types--Object/main.js
@@ -0,0 +1,15 @@
+/*
+
+Codewars, 8k: Training JS #5: Basic data types--Object
+
+Give you a function animal, accept 1 parameter:obj like this:
+
+{name:"dog",legs:4,color:"white"}
+and return a string like this:
+
+"This white dog has 4 legs."
+When you have finished the work, click "Run Tests" to see if your code is working properly.
+
+*/
+
+const animal = obj => `This ${obj.color} ${obj.name} has ${obj.legs} legs.`;
