39ffc6fe2f2e17ed7839ce9925584fd185335918
Wataru Hori <Bireleysaddiction@gmail.com>
2022-08-06 15:46:02 +0900

    Solved Question, 8k

---
+++ August 2022/2022-08-06/Grasshopper - Messi Goals/main.js
@@ -0,0 +1,23 @@
+/*
+
+Codewars, 8k
+
+Task
+
+Messi goal scoring statistics:
+
+Competition	Goals
+La Liga	43
+Champions League	10
+Copa del Rey	5
+
+Create these three variables and store the appropriate values using the table above:
+
+laLigaGoals
+championsLeagueGoals
+copaDelReyGoals
+
+Create a fourth variable named totalGoals that stores the sum of all of Messi's goals for this year.
+
+*/
+
