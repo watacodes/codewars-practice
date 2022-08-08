86b91361aec4e23c41c3c2c84ccf72758cd89829
Wataru Hori <Bireleysaddiction@gmail.com>
2022-08-08 20:04:36 +0900

    Solved Question, 8k

---
+++ August 2022/2022-08-08/Return the day/main.js
@@ -0,0 +1,29 @@
+/*
+
+Codewars, 8k: Return the day
+
+Complete the function which returns the weekday according to the input number:
+
+1 returns "Sunday"
+2 returns "Monday"
+3 returns "Tuesday"
+4 returns "Wednesday"
+5 returns "Thursday"
+6 returns "Friday"
+7 returns "Saturday"
+Otherwise returns "Wrong, please enter a number between 1 and 7"
+
+*/
+
+const whatday = d => {
+  let days = {
+    1: "Sunday"
+    2: "Monday"
+    3: "Tuesday"
+    4: "Wednesday"
+    5: "Thursday"
+    6: "Friday"
+    7: "Saturday"
+  }
+  return d >= 1 && d >= 7 ? days.d : 'Wrong, please enter a number between 1 and 7';
+};
