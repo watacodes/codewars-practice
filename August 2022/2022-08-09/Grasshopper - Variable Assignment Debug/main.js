d855a466bc6dd751c92fdf0f7d5e61607b4b1cf9
Wataru Hori <Bireleysaddiction@gmail.com>
2022-08-09 20:45:30 +0900

    Solved Question, 8k

---
+++ August 2022/2022-08-09/Grasshopper - Variable Assignment Debug/main.js
@@ -0,0 +1,11 @@
+/*
+
+Codewars, 8k: Grasshopper - Variable Assignment Debug
+
+Fix the variables assigments so that this code stores the string 'devLab' in the variable name.
+
+*/
+
+let a = "dev";
+let b = "Lab";
+let name = a + b;
