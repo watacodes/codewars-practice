14d68c30bf406e3003838225322a5ca2a15ee334
Wataru Hori <Bireleysaddiction@gmail.com>
2022-08-09 20:02:01 +0900

    Solved Question, 8k

---
+++ August 2022/2022-08-09/repeatIt/main.js
@@ -0,0 +1,19 @@
+/*
+
+Codewars, 8k: repeatIt
+
+Create a function that takes a string and an integer (n).
+
+The function should return a string that repeats the input string n number of times.
+
+If anything other than a string is passed in you should return "Not a string"
+
+Example
+"Hi", 2 --> "HiHi"
+1234, 5 --> "Not a string"
+
+*/
+
+const repeatIt = (str, num) => typeof str !== 'string' ? "Not a string" : str.repeat(num);
+
+console.log(repeatIt('Hi', 2))
