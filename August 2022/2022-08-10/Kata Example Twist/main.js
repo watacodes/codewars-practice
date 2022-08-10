8e460dbb6b7cce107358d5d8a149798533c99e8c
Wataru Hori <Bireleysaddiction@gmail.com>
2022-08-10 09:42:54 +0900

    Solved Question, 8k

---
+++ August 2022/2022-08-10/Kata Example Twist/main.js
@@ -0,0 +1,15 @@
+/*
+
+Codewars, 8k
+
+This is an easy twist to the example kata (provided by Codewars when learning how to create your own kata).
+
+Add the value "codewars" to the array websites/Websites 1,000 times.
+
+*/
+let websites = [];
+let counter = 1;
+while (counter <= 1000) {
+  websites.push('codewars');
+  counter++;
+}
