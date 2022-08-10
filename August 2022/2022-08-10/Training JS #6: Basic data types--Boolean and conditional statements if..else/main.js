a5fc561c8a030d98456a7f970307111c8b0a5ced
Wataru Hori <Bireleysaddiction@gmail.com>
2022-08-10 09:26:01 +0900

    Solved Question, 8k

---
+++ August 2022/2022-08-10/Training JS #6: Basic data types--Boolean and conditional statements if..else/main.js
@@ -0,0 +1,10 @@
+/*
+
+Codewars, 8k: Training JS #6: Basic data types--Boolean and conditional statements if..else
+
+Task
+Coding in function trueOrFalse, function accept 1 parameters:val, try to use the conditional statement if...else, if val value is false (val==false or it can convert to false), should return a string "false", if not, return a string "true".
+
+*/
+
+const trueOrFalse = v => !v ? "false" : "true";
