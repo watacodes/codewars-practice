f6db686e99719ede94fb6ef6b4c05b868b943d70
Wataru Hori <Bireleysaddiction@gmail.com>
2022-08-10 09:38:03 +0900

    Solved Question, 8k

---
+++ August 2022/2022-08-10/Are arrow function odd?/main.js
@@ -0,0 +1,13 @@
+/*
+
+Codewars, 8k: Are arrow function odd?
+
+
+Time to test your basic knowledge in functions! Return the odds from a list:
+
+[1, 2, 3, 4, 5]  -->  [1, 3, 5]
+[2, 4, 6]        -->  []
+
+*/
+
+const odds = arr => arr.filter(a => a % 2 === 1);
