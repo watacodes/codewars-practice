d7bd03ac4465d2eaaca440d12e4258154a3b0875
Wataru Hori <Bireleysaddiction@gmail.com>
2022-08-11 20:30:53 +0900

    Solved Question, 8k

---
+++ August 2022/2022-08-11/get ascii value of character/main.js
@@ -0,0 +1,11 @@
+/*
+
+Codewars, 8k: get ascii value of character
+
+Get ASCII value of a character.
+
+For the ASCII table you can refer to http://www.asciitable.com/
+
+*/
+
+const getASCII = c => c.charCodeAt();
