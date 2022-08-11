0e684656acc7bb1da59e7708df25752e41fb99a9
Wataru Hori <Bireleysaddiction@gmail.com>
2022-08-11 20:33:49 +0900

    Solved Question, 8k

---
+++ August 2022/2022-08-11/Leonardo Dicaprio and Oscars/main.js
@@ -0,0 +1,24 @@
+/*
+
+Codewars, 8k: Leonardo Dicaprio and Oscars
+
+You have to write a function that describe Leo:
+
+function leo(oscar) {
+
+}
+
+if oscar was (integer) 88, you have to return "Leo finally won the oscar! Leo is happy".
+if oscar was 86, you have to return "Not even for Wolf of wallstreet?!"
+if it was not 88 or 86 (and below 88) you should return "When will you give Leo an Oscar?"
+if it was over 88 you should return "Leo got one already!"
+
+
+*/
+
+const leo = w => {
+  if (w === 88) return "Leo finally won the oscar! Leo is happy"
+  if (w == 86) return "Not even for Wolf of wallstreet?!";
+  if (w < 88) return "When will you give Leo an Oscar?";
+  return "Leo got one already!";
+};
