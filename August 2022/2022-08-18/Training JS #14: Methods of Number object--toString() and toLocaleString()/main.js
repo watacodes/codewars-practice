c2444a5dac260236feda1d1c4644b3913ab3e6f2
Wataru Hori <Bireleysaddiction@gmail.com>
2022-08-18 06:29:47 +0900

    Solved Question, 8k

---
+++ August 2022/2022-08-18/Training JS #14: Methods of Number object--toString() and toLocaleString()/main.js
@@ -0,0 +1,28 @@
+/*
+
+Codewars, 8k: Training JS #14: Methods of Number object--toString() and toLocaleString()
+
+Task
+Coding in function colorOf. function accept 3 parameter:r g b. It means value of color red green and blue. the value range is 0-255.
+
+Use toString(16) Convert numbers r g b to hex string form. at last, combine them to a web color code and return it.
+
+the color code should starting with "#". and then use 2 characters per color.
+
+for example:
+
+colorOf(255,0,0) should return "#ff0000"
+colorOf(0,111,0) should return "#006f00"
+colorOf(1, 2 ,3) should return "#010203"
+
+*/
+
+const colorOf = (r, g, b) => {
+  let red = r.toString(16);
+  let green = g.toString(16);
+  let blue = b.toString(16);
+  red = red.length < 2 ? '0' + red : red;
+  green = green.length < 2 ? '0' + green : green;
+  blue = blue.length < 2 ? '0' + blue : blue;
+  return "#" + red + green + blue;
+} 
