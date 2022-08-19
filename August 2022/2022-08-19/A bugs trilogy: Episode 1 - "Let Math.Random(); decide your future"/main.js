e73b0a94d36ff478d54c7b632692a95b5854360d
Wataru Hori <Bireleysaddiction@gmail.com>
2022-08-19 22:25:48 +0900

    Solved Question, 8k

---
+++ August 2022/2022-08-19/A bugs trilogy: Episode 1 - "Let Math.Random(); decide your future"/main.js
@@ -0,0 +1,29 @@
+/*
+
+Codewars, 8k: A bugs trilogy: Episode 1 - "Let Math.Random(); decide your future"
+
+Our friend Pac has finally decided to pursue a career in the coding industry.
+He is a newbie, he needs to learn properly.
+Therefore, Pac wants to apply for the worldwide famous -and very demanding-
+'C0d3r 1ns1d3' Academy for beginners.
+In order to join, Pac is required to solve a series of 3 exercises about 'Bug Fixes'.
+He has been sent an email from the Academy with the following instructions,
+
+Dear Pac,  
+This is the first exercise. Find out what's wrong and fix the code.  
+You have 15 minutes to send a solution.  
+Good Luck.
+This code is a mess! Would you help Pac to fix the code in time?
+
+*/
+
+function yourFutureCareer() {
+  let career = Math.random();
+  if (career <= 0.32) {
+      return 'FrontEnd Developer';
+  }
+  if (career <= 0.65) {
+      return 'BackEnd Developer';
+  }
+  return 'Full-Stack Developer';
+}
