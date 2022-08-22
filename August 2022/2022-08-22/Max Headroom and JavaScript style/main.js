0d89ef3da43d9330f12090cad8b401db10c5e1a7
Wataru Hori <Bireleysaddiction@gmail.com>
2022-08-23 02:23:36 +0900

    Solved Question, 8k

---
+++ August 2022/2022-08-22/Max Headroom and JavaScript style/main.js
@@ -0,0 +1,21 @@
+/*
+
+Codewars, 8k: Max Headroom and JavaScript style
+
+Shouldn't the two functions getMax1 and getMax2 be equivalent and return the same value? Can you spot a problem and fix it? Can you learn something about JavaScript style in this kata?
+
+*/
+
+function getMax1() {
+  var max = {
+   name: 'Max Headroom'
+  }
+  return max;
+}
+
+function getMax2() {
+  let max = {
+    name: 'Max Headroom'
+  }
+  return max;
+}
