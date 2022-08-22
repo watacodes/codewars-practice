dbea1645b903ff6686beb7b1836b94c3fdf79c5d
Wataru Hori <Bireleysaddiction@gmail.com>
2022-08-22 19:34:42 +0900

    Solved Question, 8k

---
+++ August 2022/2022-08-22/Mr. Freeze/main.js
@@ -0,0 +1,9 @@
+/*
+
+Codewars, 8k: Mr. Freeze
+
+There is an object/class already created called MrFreeze. Mark this object as frozen so that no other changes can be made to it.
+
+*/
+
+Object.freeze(MrFreeze);
