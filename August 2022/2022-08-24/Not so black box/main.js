9f86f09780eecee977916d0deb8a8f5844995e5f
Wataru Hori <Bireleysaddiction@gmail.com>
2022-08-24 21:25:07 +0900

    Solved Question, 8k

---
+++ August 2022/2022-08-24/Not so black box/main.js
@@ -0,0 +1,11 @@
+/*
+
+Codewars, 8k: Not so black box
+
+The kata has been preloaded with a function called blackBox. Figure out what the function does.
+
+You may need to think laterally!
+
+*/
+
+JSopenSesame();
