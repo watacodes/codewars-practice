a2c2e0e6262082afa7473a997473344ee2bb9751
Wataru Hori <Bireleysaddiction@gmail.com>
2022-08-28 22:33:37 +0900

    Solved Question, 7k

---
+++ August 2022/2022-08-28/Beginner Series #3 Sum of Numbers/main.js
@@ -0,0 +1,34 @@
+/*
+
+Codewars, 7k: Beginner Series #3 Sum of Numbers
+
+Given two integers a and b, which can be positive or negative, find the sum of all the integers between and including them and return it. If the two numbers are equal return a or b.
+
+Note: a and b are not ordered!
+
+Examples (a, b) --> output (explanation)
+(1, 0) --> 1 (1 + 0 = 1)
+(1, 2) --> 3 (1 + 2 = 3)
+(0, 1) --> 1 (0 + 1 = 1)
+(1, 1) --> 1 (1 since both are same)
+(-1, 0) --> -1 (-1 + 0 = -1)
+(-1, 2) --> 2 (-1 + 0 + 1 + 2 = 2)
+
+*/
+
+const getSum = (a, b) => {
+  let first = a;
+  let last = b;
+  let counter = 0;
+  if (a > b) {
+    last = a;
+    first = b;
+  };
+  if (first === last) return first;
+  for (let i = first; i <= last; i++) {
+    counter += i;
+  };
+  return counter;
+};
+
+console.log(getSum(0, -1));
