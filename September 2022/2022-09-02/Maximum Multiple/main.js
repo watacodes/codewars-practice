352a7a252f1c4f3a1767fde1b83913d13d1cc1bc
Wataru Hori <Bireleysaddiction@gmail.com>
2022-09-02 21:23:42 +0900

    Solved Question, 7k

---
+++ September 2022/2022-09-02/Maximum Multiple/main.js
@@ -0,0 +1,25 @@
+/*
+
+Codewars, 7k: Maximum Multiple
+
+Task
+Given a Divisor and a Bound , Find the largest integer N , Such That ,
+
+Conditions :
+N is divisible by divisor
+
+N is less than or equal to bound
+
+N is greater than 0.
+
+Notes
+The parameters (divisor, bound) passed to the function are only positive values .
+It's guaranteed that a divisor is Found .
+Input >> Output Examples
+maxMultiple (2,7) ==> return (6)
+Explanation:
+(6) is divisible by (2) , (6) is less than or equal to bound (7) , and (6) is > 0 .
+
+*/
+
+const maxMultiple = (divisor, bound) => Math.floor(bound / divisor) * divisor;
