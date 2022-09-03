7ba28f327d45705c6c28cada26d1d5894f4f5258
Wataru Hori <Bireleysaddiction@gmail.com>
2022-09-03 20:41:40 +0900

    Solved Question, 7k

---
+++ September 2022/2022-09-01/Remove anchor from URL/main.js
@@ -0,0 +1,15 @@
+/*
+
+Codewars, 7k: Remove anchor from URL
+
+Complete the function/method so that it returns the url with anything after the anchor (#) removed.
+
+Examples
+"www.codewars.com#about" --> "www.codewars.com"
+"www.codewars.com?page=1" -->"www.codewars.com?page=1"
+
+*/
+
+const removeUrlAnchor = url => url.split('#')[0];
+
+console.log(removeUrlAnchor("www.codewars.com#about"))
