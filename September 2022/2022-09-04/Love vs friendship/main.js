c03da2e015c2fd618295cbdddc19cbc056b1dcd4
Wataru Hori <Bireleysaddiction@gmail.com>
2022-09-04 18:38:28 +0900

    Solved Question, 7k

---
+++ September 2022/2022-09-04/Love vs friendship/main.js
@@ -0,0 +1,51 @@
+/*
+
+Codewars, 7k: Love vs friendship
+
+If　a = 1, b = 2, c = 3 ... z = 26
+
+Then l + o + v + e = 54
+
+and f + r + i + e + n + d + s + h + i + p = 108
+
+So friendship is twice stronger than love :-)
+
+The input will always be in lowercase and never be empty.
+
+
+*/
+
+const wordsToMarks = str => {
+  const alphabet = {
+    a: 1,
+    b: 2,
+    c: 3,
+    d: 4,
+    e: 5,
+    f: 6,
+    g: 7,
+    h: 8,
+    i: 9,
+    j: 10,
+    k: 11,
+    l: 12,
+    m: 13,
+    n: 14,
+    o: 15,
+    p: 16,
+    q: 17,
+    r: 18,
+    s: 19,
+    t: 20,
+    u: 21,
+    v: 22,
+    w: 23,
+    x: 24,
+    y: 25,
+    z: 26
+  };
+  let strArr = str.toLowerCase().split('');
+  return strArr.reduce((a, b, i) => a + alphabet[b], 0);
+};
+
+console.log(wordsToMarks('love'))
