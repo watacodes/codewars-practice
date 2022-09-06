3068cbf89acf40465eca0d9207c8163e4ffc23ca
Wataru Hori <Bireleysaddiction@gmail.com>
2022-09-06 11:12:45 +0900

    Solved Question, 7k

---
+++ September 2022/2022-09-06/Number of Decimal Digits/main.js
@@ -0,0 +1,13 @@
+/*
+
+Codewars, 7k: Number of Decimal Digits
+
+Determine the total number of digits in the integer (n>=0) given as input to the function. For example, 9 is a single digit, 66 has 2 digits and 128685 has 6 digits. Be careful to avoid overflows/underflows.
+
+All inputs will be valid.
+
+*/
+
+const digits = d => d.toString().split('').length;
+
+console.log(digits(5));
