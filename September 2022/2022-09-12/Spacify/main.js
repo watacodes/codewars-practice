c6173ad7c8f6bcb3a5e0f524cd9e4407585b1b06
Wataru Hori <Bireleysaddiction@gmail.com>
2022-09-12 08:57:52 +0900

    Solved Question, 7k

---
+++ September 2022/2022-09-12/Spacify/main.js
@@ -0,0 +1,13 @@
+/*
+
+Codewars, 7k: Spacify
+
+Modify the spacify function so that it returns the given string with spaces inserted between each character.
+
+spacify("hello world") // returns "h e l l o   w o r l d"
+
+*/
+
+const spacify = s => s.split('').join(' ');
+
+console.log(spacify("Hello world"))
