c1b1d62c880d55182619959d67b2a9ae48d7aed6
Wataru Hori <Bireleysaddiction@gmail.com>
2022-09-12 09:14:15 +0900

    Solved Question, 7k

---
+++ September 2022/2022-09-12/Maximum Triplet Sum (Array Series #7)/main.js
@@ -0,0 +1,30 @@
+/*
+
+Codewars, 7k: Maximum Triplet Sum (Array Series #7)
+
+Task
+Given an array/list [] of n integers , find maximum triplet sum in the array Without duplications .
+
+Notes :
+Array/list size is at least 3 .
+
+Array/list numbers could be a mixture of positives , negatives and zeros .
+
+Repetition of numbers in the array/list could occur , So (duplications are not included when summing).
+
+Input >> Output Examples
+1- maxTriSum ({3,2,6,8,2,3}) ==> return (17)
+Explanation:
+As the triplet that maximize the sum {6,8,3} in order , their sum is (17)
+
+Note : duplications are not included when summing , (i.e) the numbers added only once .
+
+
+*/
+
+const maxTriSum = n => {
+  let sorted = [...new Set(n)].sort((a, b) => b - a);
+  return sorted[0] + sorted[1] + sorted[2];
+};
+
+console.log(maxTriSum([3,2,6,8,2,3]))
