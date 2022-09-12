516e108756010d089a3b7d8d51e4c026c8c963f7
Wataru Hori <Bireleysaddiction@gmail.com>
2022-09-12 10:25:37 +0900

    Solved Question, 7k

---
+++ September 2022/Functional Addition/main.js
@@ -0,0 +1,17 @@
+/*
+
+Codewars, 7k: Functional additions
+
+Create a function add(n)/Add(n) which returns a function that always adds n to any number
+
+Note for Java: the return type and methods have not been provided to make it a bit more challenging.
+
+var addOne = add(1);
+addOne(3); // 4
+
+var addThree = add(3);
+addThree(3); // 6
+
+*/
+
+const add = n => x => n + x;
