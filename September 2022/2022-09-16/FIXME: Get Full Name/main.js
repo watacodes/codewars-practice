0bdcfcce393c2da3de4d8b56f1e32f0824761970
Wataru Hori <Bireleysaddiction@gmail.com>
2022-09-16 18:24:29 +0900

    Solved Question, 7k

---
+++ September 2022/2022-09-16/FIXME: Get Full Name/main.js
@@ -0,0 +1,33 @@
+/*
+
+Codewars, 7k: FIXME: Get Full Name
+
+The code provided is supposed return a person's Full Name given their first and last names.
+
+But it's not working properly.
+
+Notes
+The first and/or last names are never null, but may be empty.
+
+*/
+
+class Dinglemouse {
+  
+  constructor(firstName, lastName) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+  
+  getFullName() {
+    if (!this.firstName && !this.lastName) {
+      return '';
+    } else if (!this.lastName) {
+      return this.firstName;
+    } else if (!this.firstName) {
+      return this.lastName;
+    } else {
+      return `${this.firstName} ${this.lastName}`;
+    }
+  }
+
+}
