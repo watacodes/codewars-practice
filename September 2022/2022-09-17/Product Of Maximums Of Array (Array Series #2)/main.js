76be7052aac1583dddf6380a9ab459decdd87901
Wataru Hori <Bireleysaddiction@gmail.com>
2022-09-17 21:32:52 +0900

    Solved Question, 7k

---
+++ September 2022/2022-09-17/Product Of Maximums Of Array (Array Series #2)/main.js
@@ -0,0 +1,27 @@
+/*
+
+Codewars, 7k: Product Of Maximums Of Array (Array Series #2)
+
+Task
+Given an array/list [] of integers , Find the product of the k maximal numbers.
+
+Notes
+Array/list size is at least 3 .
+
+Array/list's numbers Will be mixture of positives , negatives and zeros
+
+Repetition of numbers in the array/list could occur.
+
+Input >> Output Examples
+maxProduct ({4, 3, 5}, 2) ==>  return (20)
+Explanation:
+Since the size (k) equal 2 , then the subsequence of size 2 whose gives product of maxima is 5 * 4 = 20 .
+maxProduct ({8, 10 , 9, 7}, 3) ==>  return (720)
+Explanation:
+Since the size (k) equal 3 , then the subsequence of size 3 whose gives product of maxima is  8 * 9 * 10 = 720 .
+
+*/
+
+const maxProduct = (num, size) => [...num].sort((a, b) => b - a).slice(0, size).reduce((a, b) => a * b, 1);
+
+console.log(maxProduct([4, 3, 5], 2))
