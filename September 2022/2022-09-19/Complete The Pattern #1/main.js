bf07601196ae5d644c8444a4a64aedbd48c6aef1
Wataru Hori <Bireleysaddiction@gmail.com>
2022-09-19 21:56:07 +0900

    Solved Question, 7k

---
+++ September 2022/2022-09-19/Complete The Pattern #1/main.js
@@ -0,0 +1,58 @@
+/*
+
+Codewars, 7k: Complete The Pattern #1
+
+Task:
+You have to write a function pattern which returns the following Pattern(See Pattern & Examples) upto n number of rows.
+
+Note:Returning the pattern is not the same as Printing the pattern.
+Rules/Note:
+If n < 1 then it should return "" i.e. empty string.
+There are no whitespaces in the pattern.
+Pattern:
+1
+22
+333
+....
+.....
+nnnnnn
+Examples:
+pattern(5):
+
+1
+22
+333
+4444
+55555
+pattern(11):
+
+1
+22
+333
+4444
+55555
+666666
+7777777
+88888888
+999999999
+10101010101010101010
+1111111111111111111111
+Hint: Use \n in string to jump to next line
+
+
+*/
+
+const pattern = n => {
+  let res = '';
+  for (let i = 1; i <= n; i++) {
+    for (let j = 1; j <= i; j++) {
+      res += String(i);
+    }
+    if (i !== n) {
+      res += '\n';
+    }
+  }
+  return res;
+}
+
+console.log(pattern(5));
