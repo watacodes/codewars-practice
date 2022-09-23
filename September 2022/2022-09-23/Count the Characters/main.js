0757e8d979a2700b53655c73eb352e9b1f45aa06
Wataru Hori <Bireleysaddiction@gmail.com>
2022-09-23 20:35:34 +0900

    Solved Question, 7k

---
+++ September 2022/2022-09-23/Count the Characters/main.js
@@ -0,0 +1,21 @@
+/*
+
+Codewars, 7k: Count the Characters
+
+The goal of this kata is to write a function that takes two inputs: a string and a character. The function will count the number of times that character appears in the string. The count is case insensitive.
+
+For example:
+
+countChar("fizzbuzz","z") => 4
+countChar("Fancy fifth fly aloof","f") => 5
+The character can be any alphanumeric character.
+
+
+*/
+
+const countChar = (str, letter) => {
+  let re = new RegExp(letter, 'gi');
+  return str.match(re, '') ? str.match(re, '').length : 0;
+};
+
+console.log(countChar('Fancy fifth fly aloof', 'f'))
