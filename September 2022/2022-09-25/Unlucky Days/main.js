6cf9d08414edddd30311464beb0d3a50d164750e
Wataru Hori <Bireleysaddiction@gmail.com>
2022-09-25 07:30:40 +0900

    Solved Question, 7k

---
+++ September 2022/2022-09-25/Unlucky Days/main.js
@@ -0,0 +1,31 @@
+/*
+
+Codewars, 7k: Unlucky Days
+
+
+Friday 13th or Black Friday is considered as unlucky day. Calculate how many unlucky days are in the given year.
+
+Find the number of Friday 13th in the given year.
+
+Input: Year in Gregorian calendar as integer.
+
+Output: Number of Black Fridays in the year as an integer.
+
+Examples:
+
+unluckyDays(2015) == 3
+unluckyDays(1986) == 1
+
+
+*/
+
+const unluckyDays = y => {
+  let counter = 0;
+  for (let i = 1; i <= 12; i++) {
+    let date = new Date(i + "/13/" + y);
+    if (date.getDay() === 5) {
+      counter++;
+    };
+  };
+  return counter;
+};
