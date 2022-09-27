1e82c0c8eeeae1197963878fba7d383da37fa920
Wataru Hori <Bireleysaddiction@gmail.com>
2022-09-27 17:50:02 +0900

    Solved Question, 7k

---
+++ September 2022/2022-09-27/String Merge!/main.js
@@ -0,0 +1,23 @@
+/*
+
+Codewars, 7k: String Merge!
+
+Given two words and a letter, return a single word that's a combination of both words, merged at the point where the given letter first appears in each word. The returned word should have the beginning of the first word and the ending of the second, with the dividing letter in the middle. You can assume both words will contain the dividing letter.
+
+Examples
+("hello", "world", "l")       ==>  "held"
+("coding", "anywhere", "n")   ==>  "codinywhere"
+("jason", "samson", "s")      ==>  "jasamson"
+("wonderful", "people", "e")  ==>  "wondeople"
+
+
+*/
+
+const stringMerge = (s1, s2, s3) => {
+  s1 = s1.split(s3);
+  s2 = s2.slice(s2.split('').indexOf(s3));
+  console.log(s1, s2)
+  return s1[0] + s2;
+};
+
+console.log(stringMerge("person","here", "e"))
