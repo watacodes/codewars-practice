3795ddc40294f904a431e2709ba717d3eb38aafd
Wataru Hori <Bireleysaddiction@gmail.com>
2022-09-29 23:42:04 +0900

    Solved Question, 7k

---
+++ September 2022/2022-09-29/Find the Squares/main.js
@@ -0,0 +1,18 @@
+/* 
+
+Codewars, 7k: Find the Squares
+
+Complete the function that takes an odd integer (0 < n < 1000000) which is the difference between two consecutive perfect squares, and return these squares as a string in the format "bigger-smaller"
+
+Examples
+9  -->  "25-16"
+5  -->  "9-4"
+7  -->  "16-9"
+
+*/
+
+const findSquares = n => {
+  let max = Math.ceil(n / 2);
+  let min = n - max;
+  return max ** 2 + '-' + min ** 2;
+}
