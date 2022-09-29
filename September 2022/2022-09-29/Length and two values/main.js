c5c6d16d8ffc86d5f8936d3e29022915c8b230d4
Wataru Hori <Bireleysaddiction@gmail.com>
2022-09-29 23:30:26 +0900

    Solved Question, 7k

---
+++ September 2022/2022-09-29/Length and two values/main.js
@@ -0,0 +1,24 @@
+/*
+
+Codewars, 7k: Length and two values
+
+Given an integer n and two other values, build an array of size n filled with these two values alternating.
+
+Examples
+5, true, false     -->  [true, false, true, false, true]
+10, "blue", "red"  -->  ["blue", "red", "blue", "red", "blue", "red", "blue", "red", "blue", "red"]
+0, "one", "two"    -->  []
+
+*/
+
+const alternate = (n, c1, c2) => {
+  let arr = [];
+  for (let i = 0; i < n; i++) {
+    if (i % 2 === 0) {
+      arr.push(c1)
+    } else {
+      arr.push(c2)
+    }
+  }
+  return arr;
+}
