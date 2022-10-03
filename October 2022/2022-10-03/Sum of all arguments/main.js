156667024b6ce67b880bec43e8690e05d541fbb9
Wataru Hori <Bireleysaddiction@gmail.com>
2022-10-03 20:47:35 +0900

    Solved Question, 7k

---
+++ October 2022/2022-10-03/Sum of all arguments/main.js
@@ -0,0 +1,15 @@
+/*
+
+Codewars, 7k: Sum of all arguments
+
+Write a function that finds the sum of all its arguments.
+
+eg:
+
+sum(1, 2, 3) // => 6
+sum(8, 2) // => 10
+sum(1, 2, 3, 4, 5) // => 15
+
+*/
+
+const sum = (...a) => a.reduce((a, b) => +a + +b);
