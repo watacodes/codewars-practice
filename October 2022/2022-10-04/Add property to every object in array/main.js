395e68acf4466ba841151eeec8088f97a4f4d608
Wataru Hori <Bireleysaddiction@gmail.com>
2022-10-04 23:05:00 +0900

    Solved Question, 7k

---
+++ October 2022/2022-10-04/Add property to every object in array/main.js
@@ -0,0 +1,9 @@
+/*
+
+Codewars, 7k: Add property to every object in array
+
+Your task is to add a new property usersAnswer to every object in the array questions. The value of usersAnswer should be set to null. The solution should work for array of any length.
+
+*/
+
+questions.forEach(a => a.usersAnswer = null);
