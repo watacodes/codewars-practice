3cec11e9ea46bee59f439c8d229cbc00e9b34a87
Wataru Hori <Bireleysaddiction@gmail.com>
2022-10-09 16:54:13 +0900

    Moved directories to the correct file

---
+++ October 2022/2022-10-09/Coding Meetup #12 - Higher-Order Functions Series - Find GitHub admins/main.js
@@ -0,0 +1,14 @@
+/*
+
+Codewars, 7k: Coding Meetup #12 - Higher-Order Functions Series - Find GitHub admins
+
+*/
+
+const findAdmin = (list, lang) => list.filter(person => person.language === lang && person.githubAdmin === 'yes');
+
+console.log(findAdmin([
+  { firstName: 'Harry', lastName: 'K.', country: 'Brazil', continent: 'Americas', age: 22, language: 'JavaScript', githubAdmin: 'yes' },
+  { firstName: 'Kseniya', lastName: 'T.', country: 'Belarus', continent: 'Europe', age: 49, language: 'Ruby', githubAdmin: 'no' },
+  { firstName: 'Jing', lastName: 'X.', country: 'China', continent: 'Asia', age: 34, language: 'JavaScript', githubAdmin: 'yes' },
+  { firstName: 'Piotr', lastName: 'B.', country: 'Poland', continent: 'Europe', age: 128, language: 'JavaScript', githubAdmin: 'no' }
+], 'JavaScript'));
