f66ce3f00140751a52694c3451f9ff78babdf0ec
Wataru Hori <Bireleysaddiction@gmail.com>
2022-10-12 13:06:58 +0900

    Solved Question, 7k

---
+++ October 2022/2022-10-12/Coding Meetup #14 - Higher-Order Functions Series - Order the food/main.js
@@ -0,0 +1,29 @@
+/*
+
+Codewars, 7k: Coding Meetup #14 - Higher-Order Functions Series - Order the food
+
+You will be given an array of objects representing data about developers who have signed up to attend the next coding meetup that you are organising.
+
+Your task is to return an object which includes the count of food options selected by the developers on the meetup sign-up form.
+
+*/
+
+const orderFood = list => list.reduce((acc, curr) => {
+  if (!acc[curr.meal]) {
+    acc[curr.meal] = 1;
+  } else {
+    acc[curr.meal]++;
+  }
+  return acc;
+}, {});
+
+console.log(orderFood([
+  { firstName: 'Noah', lastName: 'M.', country: 'Switzerland', continent: 'Europe', age: 19, language: 'C', 
+    meal: 'vegetarian' },
+  { firstName: 'Anna', lastName: 'R.', country: 'Liechtenstein', continent: 'Europe', age: 52, language: 'JavaScript', 
+    meal: 'standard' },
+  { firstName: 'Ramona', lastName: 'R.', country: 'Paraguay', continent: 'Americas', age: 29, language: 'Ruby', 
+    meal: 'vegan' },
+  { firstName: 'George', lastName: 'B.', country: 'England', continent: 'Europe', age: 81, language: 'C', 
+    meal: 'vegetarian' },
+]))
