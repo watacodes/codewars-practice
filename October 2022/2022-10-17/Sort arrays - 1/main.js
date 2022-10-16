ae846fa966c6473930b36b3e758fac9b05458cfa
Wataru Hori <Bireleysaddiction@gmail.com>
2022-10-17 07:53:28 +0900

    Solved Question, 7k

---
+++ October 2022/2022-10-17/Sort arrays - 1/main.js
@@ -0,0 +1,11 @@
+/*
+
+Codewars, 7k: Sort arrays - 1
+
+Just a simple sorting usage. Create a function that returns the elements of the input-array / list sorted in lexicographical order.
+
+*/
+
+sortme = function( names ){
+  return names.sort();
+}
