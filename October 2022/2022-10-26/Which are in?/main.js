c2e2af8e9b8aee34a11c033ebb9845dc57cfab4c
Wataru Hori <Bireleysaddiction@gmail.com>
2022-10-26 20:15:16 +0900

    Solved Question, 6k

---
+++ October 2022/2022-10-26/Which are in?/main.js
@@ -0,0 +1,37 @@
+/*
+
+Codewars, 6k: Which are in?
+
+Given two arrays of strings a1 and a2 return a sorted array r in lexicographical order of the strings of a1 which are substrings of strings of a2.
+
+Example 1:
+a1 = ["arp", "live", "strong"]
+
+a2 = ["lively", "alive", "harp", "sharp", "armstrong"]
+
+returns ["arp", "live", "strong"]
+
+Example 2:
+a1 = ["tarp", "mice", "bull"]
+
+a2 = ["lively", "alive", "harp", "sharp", "armstrong"]
+
+returns []
+
+Notes:
+Arrays are written in "general" notation. See "Your Test Cases" for examples in your language.
+In Shell bash a1 and a2 are strings. The return is a string where words are separated by commas.
+Beware: In some languages r must be without duplicates.
+
+*/
+
+
+const inArray = (arr1, arr2) => {
+  return arr1.filter(w => {
+    let regex = new RegExp(w, 'g');
+    return arr2.some(a => a.match(regex))
+  }).sort();
+};
+
+console.log(inArray(["arp", "live", "strong"], ["lively", "alive", "harp", "sharp", "armstrong"]
+))
