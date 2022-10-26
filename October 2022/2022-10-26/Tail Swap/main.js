986530fb5f472e3583dfac752758ac650b86686f
Wataru Hori <Bireleysaddiction@gmail.com>
2022-10-26 19:52:40 +0900

    Solved Question, 7k

---
+++ October 2022/2022-10-26/Tail Swap/main.js
@@ -0,0 +1,22 @@
+/*
+
+Codewars, 7k: Tail Swap
+
+You'll be given a list of two strings, and each will contain exactly one colon (":") in the middle (but not at beginning or end). The length of the strings, before and after the colon, are random.
+
+Your job is to return a list of two strings (in the same order as the original list), but with the characters after each colon swapped.
+
+Examples
+["abc:123", "cde:456"]  -->  ["abc:456", "cde:123"]
+["a:12345", "777:xyz"]  -->  ["a:xyz", "777:12345"]
+
+*/
+
+const tailSwap = arr => {
+  let convertedArr = arr.map(str => str.split(':'))
+  let first = convertedArr[0][0] + ':' + convertedArr[1][1];
+  let second = convertedArr[1][0] + ':' + convertedArr[0][1];
+  return [first, second];
+};
+
+console.log(tailSwap(["abc:123", "cde:456"]))
