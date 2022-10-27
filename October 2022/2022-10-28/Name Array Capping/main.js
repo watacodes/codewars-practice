8eb36baf2dd91cbdab3dcc223724ff57dda47744
Wataru Hori <Bireleysaddiction@gmail.com>
2022-10-28 07:36:08 +0900

    Solved Question, 7k

---
+++ October 2022/2022-10-28/Name Array Capping/main.js
@@ -0,0 +1,14 @@
+/*
+
+Codewars, 7k: Name Array Capping
+
+Create a method that accepts an array of names, and returns an array of each name with its first letter capitalized.
+
+example
+
+capMe(['jo', 'nelson', 'jurie'])     // returns ['Jo', 'Nelson', 'Jurie']
+capMe(['KARLY', 'DANIEL', 'KELSEY']) // returns ['Karly', 'Daniel', 'Kelsey']
+
+*/
+
+const capMe = arr => arr.map(name => name[0].toUpperCase() + name.slice(1).toLowerCase());
