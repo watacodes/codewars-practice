ddf8bf749d87b8e3d9a72badf3e097e354b95da7
Wataru Hori <Bireleysaddiction@gmail.com>
2022-10-27 20:36:56 +0900

    Moved directories

---
+++ October 2022/2022-10-27/Return a string's even characters/main.js
@@ -0,0 +1,14 @@
+/*
+
+Codewars, 7k: Return a string's even characters.
+
+Write a function that returns a sequence (index begins with 1) of all the even characters from a string. If the string is smaller than two characters or longer than 100 characters, the function should return "invalid string".
+
+For example:
+
+"abcdefghijklm" --> ["b", "d", "f", "h", "j", "l"]
+"a"             --> "invalid string"
+
+*/
+
+const evenChars = str => str.length < 2 || str.length > 100 ? 'invalid string' : str.split('').filter((_, i) => i % 2 === 1);
