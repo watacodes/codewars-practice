fc3758bc881a69c5b122428361f551e5b9b2af65
Wataru Hori <Bireleysaddiction@gmail.com>
2022-10-27 19:31:48 +0900

    Solved Question, 7k

---
+++ October 2022/2022-10-27/Series of integers from 0 to n/main.js
@@ -0,0 +1,19 @@
+/*
+
+Codewars, 7k: Series of integers from 0 to n
+
+Write a function generateIntegers/generate_integers that accepts a single argument n/$n and generates an array containing the integers from 0 to n/$n inclusive.
+
+For example, generateIntegers(3)/generate_integers(3) should return [0, 1, 2, 3].
+
+n/$n can be any integer greater than or equal to 0.
+
+*/
+
+const generateIntegers = n => {
+  let arr = [];
+  for (let i = 0; i <= n; i++) {
+    arr.push(i);
+  }
+  return arr;
+};
