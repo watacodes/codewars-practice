8c8340904c7f65645087880f9149db7319330586
Wataru Hori <Bireleysaddiction@gmail.com>
2022-10-29 19:12:12 +0900

    Solved Question, 7k

---
+++ October 2022/2022-10-29/Powers of i/main.js
@@ -0,0 +1,15 @@
+/*
+
+Codewars, 7k: Powers of i
+
+i is the imaginary unit, it is defined by i²=−1i² = -1i²=−1, therefore it is a solution to x² + 1 = 0
+
+Your Task
+Complete the function pofi that returns iii to the power of a given non-negative integer in its simplest form, as a string (answer may contain iii).
+
+*/
+
+const pofi = n => {
+  let arr = ['1', 'i', '-1', '-i'];
+  return arr[n % 4];
+};
