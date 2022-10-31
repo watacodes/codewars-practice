bcab1ccce0a6baf5f7d0d5acd747bee865db042a
Wataru Hori <Bireleysaddiction@gmail.com>
2022-10-31 20:04:08 +0900

    Solved Question, 7k

---
+++ October 2022/2022-10-31/Simple Fun #63: Shape Area/main.js
@@ -0,0 +1,14 @@
+/*
+
+Codewars, 7k: Simple Fun #63: Shape Area
+
+n = 1 => 1     4 -> 9 -> 12 
+n = 2 => 5
+n = 3 => 13
+n = 4 => 25
+
+The formula = n ** 2 + (n - 1) ** 2
+
+*/
+
+const shapeArea = n => n ** 2 + (n - 1) ** 2;
