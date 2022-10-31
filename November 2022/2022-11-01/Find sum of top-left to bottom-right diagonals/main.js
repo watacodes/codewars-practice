1173847fc69ea9f3b3aca14d6ed2eb67eddb78e5
Wataru Hori <Bireleysaddiction@gmail.com>
2022-11-01 07:26:12 +0900

    Solved Question, 7k

---
+++ November 2022/2022-11-01/Find sum of top-left to bottom-right diagonals/main.js
@@ -0,0 +1,33 @@
+/*
+
+Codewars, 7k: 
+
+Given a square matrix (i.e. an array of subarrays), find the sum of values from the first value of the first array, the second value of the second array, the third value of the third array, and so on...
+
+Examples
+array = [[1, 2],
+         [3, 4]]
+
+diagonal sum: 1 + 4 = 5
+array = [[5, 9, 1, 0],
+         [8, 7, 2, 3],
+         [1, 4, 1, 9],
+         [2, 3, 8, 2]]
+
+diagonal sum: 5 + 7 + 1 + 2 = 15
+
+*/
+
+const diagonalSum = matrix => {
+  let res = 0;
+  for (let row = 0; row < matrix.length; row++) {
+    res += matrix[row][row];
+  };
+  return res;
+};
+
+console.log(diagonalSum([[5, 9, 1, 0],
+  [8, 7, 2, 3],
+  [1, 4, 1, 9],
+  [2, 3, 8, 2]]))
+
