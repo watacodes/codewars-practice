b2a149e2314f60c19e074b5ac3a6f283ae251909
Wataru Hori <Bireleysaddiction@gmail.com>
2022-11-02 13:03:42 +0900

    Solved Question, 7k

---
+++ November 2022/2022-11-02/Capitals first!/main.js
@@ -0,0 +1,33 @@
+/*
+
+Codewars, 7k: Capitals first!
+
+Create a function that takes an input String and returns a String, where all the uppercase words of the input String are in front and all the lowercase words at the end. The order of the uppercase and lowercase words should be the order in which they occur.
+
+If a word starts with a number or special character, skip the word and leave it out of the result.
+
+Input String will not be empty.
+
+For an input String: "hey You, Sort me Already!" the function should return: "You, Sort Already! hey me"
+
+*/
+
+const capitalsFirst = str => {
+  let sorted = str.split(' ');
+  let capitalized = [];
+  let lower = []
+  for (let i of sorted) {
+    if (!i.match(/^[0-9]/g)) {
+      if (i.charAt(0).match(/^[A-Z]/)) {
+        capitalized.push(i);
+      } else if (i.match(/^\w/)) {
+        lower.push(i);
+      }
+    }
+  }
+  return capitalized.concat(lower).join(' ');
+};
+
+
+
+console.log(capitalsFirst("hey You, Sort me Already!"));
