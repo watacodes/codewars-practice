bd3723d7f0182c1f5971597c32847dbe02596084
Wataru Hori <Bireleysaddiction@gmail.com>
2022-11-03 19:54:45 +0900

    Solved Question, 7k

---
+++ November 2022/2022-11-03/Working with arrays I (and why your code fails in some katas)/main.js
@@ -0,0 +1,9 @@
+/*
+
+Codewars, 7k: Working with arrays I (and why your code fails in some katas)
+
+In this kata the function returns an array/list of numbers without its last element. The function is already written for you and the basic tests pass, but random tests fail. Your task is to figure out why and fix it.
+
+*/
+
+const withoutLast = arr => arr.slice(0, -1);
