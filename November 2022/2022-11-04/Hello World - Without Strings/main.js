937c35182ec09e88462e106acd54d8601d104449
Wataru Hori <Bireleysaddiction@gmail.com>
2022-11-04 21:27:22 +0900

    Solved Question, 7k

---
+++ November 2022/2022-11-04/Hello World - Without Strings/main.js
@@ -0,0 +1,13 @@
+/*
+
+Codewars, 7k: Hello World - Without Strings
+
+
+*/
+
+const helloWorld = () => {
+  return String.fromCharCode(72, 101, 108, 108, 111, 44, 32, 87, 111, 114, 108, 100, 33);
+}
+
+console.log(helloWorld());
+console.log(String.fromCharCode(97));
