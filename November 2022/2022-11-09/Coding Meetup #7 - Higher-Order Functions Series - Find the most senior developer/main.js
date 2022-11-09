726d522cc46b9ffe84f77027259e6d99135571d9
Wataru Hori <Bireleysaddiction@gmail.com>
2022-11-09 20:22:36 +0900

    Solved Question, 6k

---
+++ November 2022/2022-11-09/Coding Meetup #7 - Higher-Order Functions Series - Find the most senior developer/main.js
@@ -0,0 +1,26 @@
+/*
+
+Codewars, 7k: Coding Meetup #7 - Higher-Order Functions Series - Find the most senior developer
+
+
+*/
+var list1 = [
+  { firstName: 'Gabriel', lastName: 'X.', country: 'Monaco', continent: 'Europe', age: 49, language: 'PHP' },
+  { firstName: 'Odval', lastName: 'F.', country: 'Mongolia', continent: 'Asia', age: 38, language: 'Python' },
+  { firstName: 'Emilija', lastName: 'S.', country: 'Lithuania', continent: 'Europe', age: 19, language: 'Python' },
+  { firstName: 'Sou', lastName: 'B.', country: 'Japan', continent: 'Asia', age: 49, language: 'PHP' },
+];
+
+const findSenior = arr => {
+  const ageArr = arr.map(a => a.age)
+  const oldestArr = [];
+  for (let i = 0; i < ageArr.length; i++) {
+    if (ageArr[i] === Math.max(...ageArr)) {
+      oldestArr.push(i);
+    }
+  }
+  console.log(oldestArr, ageArr);
+  return arr.filter((a, i) => oldestArr.includes(i));
+}
+
+console.log(findSenior(list1));
