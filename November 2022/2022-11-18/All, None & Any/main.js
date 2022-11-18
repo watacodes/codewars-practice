5c33328d851de92d74904f3065eec9e57172a2b7
Wataru Hori <Bireleysaddiction@gmail.com>
2022-11-18 20:38:45 +0900

    Solved Question, 7k

---
+++ November 2022/2022-11-18/All, None & Any/main.js
@@ -0,0 +1,17 @@
+/*
+
+Codewars, 7k: All, None & Any
+
+*/
+
+Array.prototype.all = function(p) {
+  return this.filter(p).length === this.length;
+};
+
+Array.prototype.none = function (p) {
+  return this.filter(p).length === 0;
+};
+
+Array.prototype.any = function (p) {
+  return this.filter(p).length >= 1;
+};
