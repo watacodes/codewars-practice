9baaf2894c2719da1da540480e246c0d3beb61e2
Wataru Hori <Bireleysaddiction@gmail.com>
2022-11-24 20:35:36 +0900

    Solved Question, 7k

---
+++ November 2022/2022-11-24/NATO Phonetic Alphabet/main.js
@@ -0,0 +1,22 @@
+/*
+
+Codewars, 7k: NATO Phonetic Alphabet
+
+*/
+
+const nato = str => {
+  const letters = {
+    "A": "Alpha",  "B": "Bravo",   "C": "Charlie",
+    "D": "Delta",  "E": "Echo",    "F": "Foxtrot",
+    "G": "Golf",   "H": "Hotel",   "I": "India",
+    "J": "Juliett","K": "Kilo",    "L": "Lima",
+    "M": "Mike",   "N": "November","O": "Oscar",
+    "P": "Papa",   "Q": "Quebec",  "R": "Romeo",
+    "S": "Sierra", "T": "Tango",   "U": "Uniform",
+    "V": "Victor", "W": "Whiskey", "X": "X-ray",
+    "Y": "Yankee", "Z": "Zulu"
+  }
+  return [...str].map(alpha => letters[alpha.toUpperCase()]).join(' ');
+}
+
+console.log(nato('hi'));
