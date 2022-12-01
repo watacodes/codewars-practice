f91d8551cd32f9fa915d1b329cf9ebde9b02dbde
Wataru Hori <Bireleysaddiction@gmail.com>
2022-12-01 22:22:40 +0900

    Solved Question, 7k

---
+++ December 2022/2022-12-01/Calculate Meal Total/main.js
@@ -0,0 +1,9 @@
+/*
+
+Codewars, 7k: Calculate Meal Total
+
+*/
+
+function calculate_total(subtotal, tax, tip) {
+  return +((subtotal + subtotal * (tax / 100) + subtotal * (tip / 100)).toFixed(2));
+}
