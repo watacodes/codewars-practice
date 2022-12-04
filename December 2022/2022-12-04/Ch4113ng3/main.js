35c4e915e9a269c3c65f206daea5f6b181638504
Wataru Hori <Bireleysaddiction@gmail.com>
2022-12-04 19:47:02 +0900

    Solved Question, 7k

---
+++ December 2022/2022-12-04/Ch4113ng3/main.js
@@ -0,0 +1,12 @@
+/*
+
+Codewars, 7k: Ch4113ng3
+
+*/
+
+const nerdify = str => {
+  return str.replace(/[a]/gi, '4').replace(/[l]/g, '1').replace(/[e]/gi, '3');
+}
+
+
+console.log(nerdify("Seven"))
