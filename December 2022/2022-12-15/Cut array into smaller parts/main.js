05f3aeb96eabf14e0f99f3de306f5c379dfe6b5a
Wataru Hori <Bireleysaddiction@gmail.com>
2022-12-15 13:42:47 +0900

    Solved question, 7k

---
+++ December 2022/2022-12-15/Cut array into smaller parts/main.js
@@ -0,0 +1,13 @@
+/*
+
+Codewars, 7k: Cut array into smaller parts
+
+*/
+
+const makeParts = (arr, size) => {
+  const res = [];
+  for (let i = 0; i < arr.length; i += size) {
+    res.push(arr.slice(i, i + size));
+  }
+  return res;
+}
