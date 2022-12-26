bcab68b313983bbd266b63e28f1957ebb8bd34fb
Wataru Hori <Bireleysaddiction@gmail.com>
2022-12-26 19:10:43 +0900

    Moved files

---
+++ December 2022/2022-12-26/Simple Fun #22: Is Smooth?/main.js
@@ -0,0 +1,13 @@
+/*
+
+Codewars, 7k: Simple Fun #22: Is Smooth?
+
+*/
+
+const isSmooth = arr => {
+  const middle = arr.length % 2 
+            ? arr.slice(Math.floor(arr.length / 2), Math.floor(arr.length / 2) + 1)
+            : arr.slice(Math.floor(arr.length / 2) - 1, Math.floor(arr.length / 2) + 1);
+  const middleSum = middle.reduce((acc, curr) => acc + curr, 0);
+  return middleSum === arr[0] && middleSum === arr.slice(-1)[0];
+}
