f949ed104fc87fa3961501fb39e1916b401e29ee
Wataru Hori <Bireleysaddiction@gmail.com>
2023-01-06 17:37:57 +0900

    Solved question, 7k

---
+++ January 2023/2023-01-06/Sentences with Functions/main.js
@@ -0,0 +1,45 @@
+/*
+
+Codewars, 7k: Sentences with functions
+
+*/
+
+function Adam(args) {
+  return args === undefined ? 'Adam.' : `Adam ${args}`;
+}
+
+function has(args) {
+  return args === undefined ? 'has.' : `has ${args}`;
+}
+
+function a(args) {
+  return args === undefined ? 'a.' : `a ${args}`;
+}
+
+function dog(args) {
+  return args === undefined ? 'dog.' : `dog ${args}`;
+}
+
+function The(args) {
+  return args === undefined ? 'The.' : `The ${args}`;
+}
+
+function name(args) {
+  return args === undefined ? 'name.' : `name ${args}`;
+}
+
+function of(args) {
+  return args === undefined ? 'of.' : `of ${args}`;
+}
+
+function the(args) {
+  return args === undefined ? 'the.' : `the ${args}`;
+}
+
+function is(args) {
+  return args === undefined ? 'is.' : `is ${args}`;
+}
+
+function also(args) {
+  return args === undefined ? 'also.' : `also ${args}`;
+}
