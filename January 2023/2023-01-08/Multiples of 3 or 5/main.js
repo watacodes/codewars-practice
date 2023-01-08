71e0196a457f0a44c263dcd75bb7e5f0be073b77
Wataru Hori <bireleysaddiction@gmail.com>
2023-01-08 14:12:06 +0900

    Modified file

--- January 2023/2023-01-08/Multiples of 3 or 5/main.js
+++ January 2023/2023-01-08/Multiples of 3 or 5/main.js
@@ -4,3 +4,11 @@ Codewars, 7k: Multiples of 3 or 5
 
 */
 
+function solution(number) {
+  const res = [];
+  for (let i = 1; i < number; i++) {
+    if (i % 3 === 0 || i % 5 === 0) 
+      res.push(i);
+  }
+  return res.reduce((acc, curr) => acc + curr, 0);
+}
