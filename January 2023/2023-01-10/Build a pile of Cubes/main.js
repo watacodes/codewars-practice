9831ec128bc99e33a1321dfac1844b15fb699ab5
Wataru Hori <Bireleysaddiction@gmail.com>
2023-01-10 14:06:01 +0900

    Saved file

--- January 2023/2023-01-10/Build a pile of Cubes/main.js
+++ January 2023/2023-01-10/Build a pile of Cubes/main.js
@@ -4,3 +4,11 @@ Codewars, 6k: Build a pile of Cubes
 
 */
 
+function findNb(m, i = 1) {
+  let tempVol = 0;
+  while (tempVol + i ** 3 < m) {
+    ++i;
+    tempVol += i ** 3;
+  }
+  return tempVol + 1 === m ? i : -1;
+}
