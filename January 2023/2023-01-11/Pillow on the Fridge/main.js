1f9f8ed1f9b7d8c74dc8a1bacb6d727111b84b3a
Wataru Hori <bireleysaddiction@gmail.com>
2023-01-11 23:03:59 +0900

    Solved question, 7k

---
+++ January 2023/2023-01-11/Pillow on the Fridge/main.js
@@ -0,0 +1,13 @@
+/*
+
+Codewars, 7k: Pillow on the Fridge
+
+*/
+
+function pillow(s) {
+  for (let i = 0; i < s[0].length; i++) {
+    if (s[0][i] === 'n' && s[1][i] === 'B')
+      return true;
+  }
+  return false;
+}
