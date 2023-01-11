edc86662d1b0fd348463dc4782936f1ae5dfa2a9
Wataru Hori <bireleysaddiction@gmail.com>
2023-01-11 23:04:22 +0900

    Updated to the saved file

--- January 2023/2023-01-11/Farthest Distance/main.js
+++ January 2023/2023-01-11/Farthest Distance/main.js
@@ -4,3 +4,13 @@ Codewars, 7k: Farthest Distance
 
 */
 
+function furthestDistance(arr){
+  const max = [];
+  for (let i = 0; i < arr.length - 1; i++) {
+    for (let k = i + 1; k < arr.length; k++) {
+      let distance = Math.sqrt((arr[i][0] - arr[k][0]) ** 2 + (arr[i][1] - arr[k][1]) ** 2);
+      max.push(Number(distance.toFixed(2)));
+    }
+  }
+  return Math.max(...max);
+}
