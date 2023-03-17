d2837f30e7c3980f10144711eab93561096f9630
Wataru Hori <Bireleysaddiction@gmail.com>
2023-03-17 23:05:57 +0900

    Solved question, 6k

--- March 2023/2023-03-17/Simple frequency sort/main.js
+++ March 2023/2023-03-17/Simple frequency sort/main.js
@@ -4,3 +4,15 @@ Codewars, 6k: Simple frequency sort
 
 */
 
+function solve(arr) {
+  const occurrence = arr.reduce((acc, curr) => {
+    if (!acc[curr]) {
+      acc[curr] = 1;
+    } else {
+      acc[curr]++;
+    }
+    return acc;
+  }, {});
+  const entries = Object.entries(occurrence).sort((a, b) => b[1] - a[1] || +a[0] - +b[0]).map(e => +e[0]);
+  return arr.sort((a, b) => entries.indexOf(a) - entries.indexOf(b));
+}
