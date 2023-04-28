52a1aaf38f74894909c44c0c900c9d3f10e9053e
Wataru Hori <Bireleysaddiction@gmail.com>
2023-04-28 21:29:33 +0900

    Solved question, 6k

---
+++ April 2023/2023-04-28/Check same case/main.py
@@ -0,0 +1,10 @@
+# Codewars, 8k: Check same case
+
+def same_case(a, b): 
+    if a.isalpha() and b.isalpha():
+        if (a.islower() and b.islower()) or (a.isupper() and b.isupper()):
+            return 1
+        else:
+            return 0
+    else:
+        return -1
