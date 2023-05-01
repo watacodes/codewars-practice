f1102f25d2f4764e247956ef5b02f97a867446ce
Wataru Hori <Bireleysaddiction@gmail.com>
2023-05-01 21:13:03 +0900

    Solved question, 6k

---
+++ May 2023/2023-05-01/Basic Mathematical Operations/main.py
@@ -0,0 +1,11 @@
+# Codewars, 8k: Basic Mathematical Operations
+
+def basic_op(op, v1, v2):
+    if op == '+':
+        return v1 + v2
+    elif op == '-':
+        return v1 - v2
+    elif op == '*':
+        return v1 * v2
+    else:
+        return v1 / v2
