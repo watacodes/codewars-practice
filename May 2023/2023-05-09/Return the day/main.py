7569b4a94c5edfca8be602a34ecbd6a6c66a846b
Wataru Hori <Bireleysaddiction@gmail.com>
2023-05-09 22:30:11 +0900

    Updated

--- May 2023/2023-05-09/Return the day/main.py
+++ May 2023/2023-05-09/Return the day/main.py
@@ -2,7 +2,7 @@
 
 def whatday(num):
     days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
-    if num < 8:
+    if num > 0 and num <= 7:
         return days[num - 1]
     else:
         return 'Wrong, please enter a number between 1 and 7'
