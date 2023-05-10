67a642315c4aa43f1db86e602e6b5d8cd9dda1f3
Wataru Hori <Bireleysaddiction@gmail.com>
2023-05-10 22:40:55 +0900

    Solved question, 6k

---
+++ May 2023/2023-05-10/Who is going to pay for the wall?/main.py
@@ -0,0 +1,10 @@
+# Codewars, 8k: Who is going to pay for the wall?
+
+def who_is_paying(name):
+    res = []
+    if len(name) <= 2:
+        res.append(name)
+    else:
+        res.append(name)
+        res.append(name[:2])
+    return res
