b032241b51f0f68c06da49dd2e9a805b536fb1ef
Wataru Hori <Bireleysaddiction@gmail.com>
2023-05-12 23:09:06 +0900

    Solved question, 6k

---
+++ May 2023/2023-05-12/For UFC Fans (Total Beginners): Conor McGregor vs George Saint Pierre/main.py
@@ -0,0 +1,4 @@
+# Codewars, 8k: For UFC Fans (Total Beginners): Conor McGregor vs George Saint Pierre
+
+def quote(fighter):
+    return 'I am not impressed by your performance.' if fighter.lower() == 'george saint pierre' else 'I\'d like to take this chance to apologize.. To absolutely NOBODY!'
