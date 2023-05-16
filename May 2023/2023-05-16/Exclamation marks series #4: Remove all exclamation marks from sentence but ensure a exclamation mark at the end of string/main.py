efcb9991506122a82740f9d4e1d55849a024aaac
Wataru Hori <Bireleysaddiction@gmail.com>
2023-05-16 23:21:05 +0900

    Solved question, 6k

---
+++ May 2023/2023-05-16/Exclamation marks series #4: Remove all exclamation marks from sentence but ensure a exclamation mark at the end of string/main.py
@@ -0,0 +1,4 @@
+# Codewars, 8k: Exclamation marks series #4: Remove all exclamation marks from sentence but ensure a exclamation mark at the end of string
+
+def remove(s):
+    return s.replace('!', '') + '!'
