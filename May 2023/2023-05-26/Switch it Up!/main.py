60ef612f175d84b4a293620d6601576797892996
Wataru Hori <Bireleysaddiction@gmail.com>
2023-05-27 00:53:32 +0900

    Solved question, 6k

---
+++ May 2023/2023-05-26/Switch it Up!/main.py
@@ -0,0 +1,17 @@
+# Codewars, 8k: Switch it Up!
+
+def switch_it_up(number):
+    num_output = {
+        0: 'Zero',
+        1: 'One',
+        2: 'Two',
+        3: 'Three',
+        4: 'Four',
+        5: 'Five',
+        6: 'Six',
+        7: 'Seven',
+        8: 'Eight',
+        9: 'Nine'
+    }
+    
+    return num_output[number]
