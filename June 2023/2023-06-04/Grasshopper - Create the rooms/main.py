79f67d51de87542c4cf0111c17e97356d8de3e98
Wataru Hori <Bireleysaddiction@gmail.com>
2023-06-04 19:45:04 +0900

    Solved question, 6k

---
+++ June 2023/2023-06-04/Grasshopper - Create the rooms/main.py
@@ -0,0 +1,19 @@
+# Codewars, 8k: Grasshopper - Create the rooms
+
+rooms = {
+        'room1': {
+            'user': 'm',
+            'msg': 'hi',
+            'completed': False
+        },
+        'room2': {
+            'user': 'm',
+            'msg': 'hi',
+            'completed': False
+        },
+        'room3': {
+            'user': 'm',
+            'msg': 'hi',
+            'completed': False
+        }
+}
