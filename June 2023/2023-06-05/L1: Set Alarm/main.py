59ed4fc6c9f56040aa84a47ebbd2ebd6c0caed14
Wataru Hori <Bireleysaddiction@gmail.com>
2023-06-06 00:42:28 +0900

    Solved question, 6k

---
+++ June 2023/2023-06-05/L1: Set Alarm/main.py
@@ -0,0 +1,9 @@
+# Codewars, 8k: L1: Set Alarm
+
+def set_alarm(employed, vacation):
+    if employed and vacation:
+        return False
+    if employed:
+        return True
+    else:
+        return False
