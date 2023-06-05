032d9a1d6e8f3fce7c47a74a2c70803f90ec82de
Wataru Hori <Bireleysaddiction@gmail.com>
2023-06-06 00:47:12 +0900

    Solved question, 6k

---
+++ June 2023/2023-06-05/Drink about/main.py
@@ -0,0 +1,10 @@
+# Codewars, 8k: Drink about
+
+def people_with_age_drink(age):
+    if age < 14:
+        return 'drink toddy'
+    elif age < 18:
+        return 'drink coke'
+    elif age < 21:
+        return 'drink beer'
+    return 'drink whisky'
