e3dbf39183acf4c8bc09d72804e135ef0c0b94f4
Wataru Hori <Bireleysaddiction@gmail.com>
2023-06-07 23:46:39 +0900

    Solved Question, 8k

---
+++ June 2023/2023-06-07/How do I compare numbers?/main.py
@@ -0,0 +1,9 @@
+# Codewars, 8k: How do I compare numbers?
+
+def what_is(x):
+    if x == 42:
+        return 'everything'
+    elif x == 42 * 42:
+        return 'everything squared'
+    else:
+        return 'nothing'
