45d89d2fa3fc3571285be489c64ec095078a2eb3
Wataru Hori <Bireleysaddiction@gmail.com>
2023-06-11 00:00:31 +0900

    Solved Question, 8k

---
+++ June 2023/2023-06-10/Fundamentals: Return/main.py
@@ -0,0 +1,14 @@
+# Codewars, 8k: Fundamentals: Return
+
+def add(a,b):
+    return a + b 
+def multiply(a,b):
+    return a * b
+def divide(a,b):
+    return a / b
+def mod(a,b):
+    return a % b
+def exponent(a,b):
+    return a ** b
+def subt(a,b):
+    return a - b
