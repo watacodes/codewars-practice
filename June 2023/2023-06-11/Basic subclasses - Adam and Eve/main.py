66f33137ddf57f767d30c49588d229e23cc7008d
Wataru Hori <Bireleysaddiction@gmail.com>
2023-06-11 17:45:54 +0900

    Solved Question, 8k

---
+++ June 2023/2023-06-11/Basic subclasses - Adam and Eve/main.py
@@ -0,0 +1,13 @@
+# Codewars, 8k: Basic subclasses - Adam and Eve
+
+def God():
+    return [Man(), Woman()]
+
+class Human():
+    pass
+
+class Man(Human):
+    pass
+
+class Woman(Human):
+    pass
