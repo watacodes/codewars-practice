ee079caf08581113962ed7df8aac7995999e72ee
Wataru Hori <Bireleysaddiction@gmail.com>
2023-06-25 23:25:27 +0900

    Moved files

---
+++ June 2023/2023-06-24/Never visit a . . . !?/main.py
@@ -0,0 +1,5 @@
+# Codewars, 8k: Never visit a . . . !?
+
+
+def subtract_sum(number):
+    return 'apple'
---
+++ June 2023/2023-06-24/Remove the time/main.py
@@ -0,0 +1,4 @@
+# Codewars, 8k: Remove the time
+
+def shorten_to_date(long_date):
+    return long_date.split(',')[0]
