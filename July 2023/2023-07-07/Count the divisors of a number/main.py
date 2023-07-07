38424eb16328ead6250cbe57969068d476bec369
Wataru Hori <Bireleysaddiction@gmail.com>
2023-07-08 00:42:39 +0900

    Solved Question, 7k

---
+++ July 2023/2023-07-07/Count the divisors of a number/main.py
@@ -0,0 +1,10 @@
+# Codewars, 7k: Count the divisors of a number
+
+def divisors(n):
+    count = 0
+    
+    for i in range(1, n + 1):
+        if n % i == 0:
+            count += 1
+    
+    return count
