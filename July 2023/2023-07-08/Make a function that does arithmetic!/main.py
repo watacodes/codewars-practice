e806c1a36f41b5bf6ca2369d52346602a9155fed
Wataru Hori <Bireleysaddiction@gmail.com>
2023-07-08 22:32:26 +0900

    Solved question, 6k

---
+++ July 2023/2023-07-08/Make a function that does arithmetic!/main.py
@@ -0,0 +1,10 @@
+# Codewars, 7k: Make a function that does arithmetic!
+
+def arithmetic(a, b, operator):
+    if operator == 'add':
+        return a + b
+    elif operator == 'subtract':
+        return a - b
+    elif operator == 'multiply':
+        return a * b
+    return a / b
