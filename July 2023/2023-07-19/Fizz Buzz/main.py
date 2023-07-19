0527cdc7a6a50829259ab2aeaec6155f35461e0b
Wataru Hori <Bireleysaddiction@gmail.com>
2023-07-20 00:26:04 +0900

    Solved question, 7k

---
+++ July 2023/2023-07-19/Fizz Buzz/main.py
@@ -0,0 +1,16 @@
+# Codewars, 7k: Fizz Buzz
+
+def fizzbuzz(n):
+    res = []
+    
+    for i in range(1, n + 1):
+        if i % 15 == 0:
+            res.append('FizzBuzz')
+        elif i % 5 == 0:
+            res.append('Buzz')
+        elif i % 3 == 0:
+            res.append('Fizz')
+        else:
+            res.append(i)
+            
+    return res
