180fea87765c789d3b5a35720fc71de9441dec4b
Wataru Hori <Bireleysaddiction@gmail.com>
2023-07-24 22:04:55 +0900

    Solved question, 7k

---
+++ July 2023/2023-07-24/Find the nth Digit of a Number/main.py
@@ -0,0 +1,13 @@
+# Codewars, 7k: Find the nth Digit of a Number
+
+def find_digit(num, nth):
+    num = str(num)
+    
+    if nth <= 0:
+        return -1
+    elif nth > len(num):
+        return 0
+    else:
+        num = num[::-1]
+    
+    return int(num[nth-1])
